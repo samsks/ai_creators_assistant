e13efd956feb81c89b3bc8cf580d9bf6873ff018
samsks <samuel.meireles.15s@outlook.com>
2023-09-19 10:54:04 -0300

    feat(backend_api):

---
+++ upload_ai_api/prisma/migrations/20230919132236_create_videos_and_prompts_tables/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "Video" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "path" TEXT NOT NULL,
+    "transcription" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Prompt" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "template" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
