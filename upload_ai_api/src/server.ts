d888747c041a909b9bd05798d502a73d53af723d
samsks <samuel.meireles.15s@outlook.com>
2023-09-19 21:09:56 -0300

    feat(backend_api): :sparkles: create post videos complete route

--- upload_ai_api/src/server.ts
+++ upload_ai_api/src/server.ts
@@ -1,9 +1,11 @@
 import { fastify } from "fastify";
-import { promptRoutes } from "./routes/prompts.routes";
+import { promptRoutes } from "./routes/prompts.route";
+import { videoRoutes } from "./routes/videos.route";
 
 const app = fastify();
 
 app.register(promptRoutes, { prefix: "/prompts" });
+app.register(videoRoutes, { prefix: "/videos" });
 
 app
   .listen({
