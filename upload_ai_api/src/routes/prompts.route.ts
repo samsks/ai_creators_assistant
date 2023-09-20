1d7909f9dd272bfad8a1f01674158be6b1c99e05
samsks <samuel.meireles.15s@outlook.com>
2023-09-19 21:04:34 -0300

    fix(backend_api): :truck: rename file and doc function

--- upload_ai_api/src/routes/prompts.route.ts
+++ upload_ai_api/src/routes/prompts.route.ts
@@ -1,6 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
 
+/**
+ * Registers the prompt routes to the Fastify instance.
+ * @param app - The Fastify instance to register the routes to.
+ */
 export async function promptRoutes(app: FastifyInstance) {
   app.get("/", async (request, reply) => {
     return await prisma.prompt.findMany();
