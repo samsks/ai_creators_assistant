71aa9e967e792e84c3b01738412249971b4e2ab7
samsks <samuel.meireles.15s@outlook.com>
2023-09-20 22:20:00 -0300

    feat(backend_api): :sparkles: add ai complete routes

--- upload_ai_api/src/server.ts
+++ upload_ai_api/src/server.ts
@@ -1,11 +1,13 @@
 import { fastify } from "fastify";
 import { promptRoutes } from "./routes/prompts.route";
 import { videoRoutes } from "./routes/videos.route";
+import { aiRoutes } from "./routes/ai.route";
 
 const app = fastify();
 
 app.register(promptRoutes, { prefix: "/prompts" });
 app.register(videoRoutes, { prefix: "/videos" });
+app.register(aiRoutes, { prefix: "/ai" });
 
 app
   .listen({
