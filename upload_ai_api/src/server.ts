ae0d7fd66b01ffd817fd999419980e66fe53ce15
samsks <samuel.meireles.15s@outlook.com>
2023-09-21 18:49:05 -0300

    feat(backend_api): :sparkles: cors

--- upload_ai_api/src/server.ts
+++ upload_ai_api/src/server.ts
@@ -2,8 +2,12 @@ import { fastify } from "fastify";
 import { promptRoutes } from "./routes/prompts.route";
 import { videoRoutes } from "./routes/videos.route";
 import { aiRoutes } from "./routes/ai.route";
+import { fastifyCors } from "@fastify/cors";
 
 const app = fastify();
+app.register(fastifyCors, {
+  origin: "*",
+});
 
 app.register(promptRoutes, { prefix: "/prompts" });
 app.register(videoRoutes, { prefix: "/videos" });
