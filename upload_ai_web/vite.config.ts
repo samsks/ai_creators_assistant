4f92851adc385634cfe2dad3e7cde7e653c7047c
samsks <samuel.meireles.15s@outlook.com>
2023-09-21 21:56:33 -0300

    feat(frontend_web): :sparkles: create videoInputForm component

--- upload_ai_web/vite.config.ts
+++ upload_ai_web/vite.config.ts
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
