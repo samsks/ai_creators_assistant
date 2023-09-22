ff24e1a27cbeec0c4040432c03742f75599bc79c
samsks <samuel.meireles.15s@outlook.com>
2023-09-22 12:27:13 -0300

    feat(frontend_web): :sparkles: dynamic select temperature ai

--- upload_ai_web/src/App.tsx
+++ upload_ai_web/src/App.tsx
@@ -13,8 +13,11 @@ import {
 import { Slider } from "./components/ui/slider";
 import { VideoInputForm } from "./components/videoInputForm";
 import { PromptSelect } from "./components/promptSelect";
+import { useState } from "react";
 
 export function App() {
+  const [temperature, setTemperature] = useState(0.5);
+
   function handlePromptSelected(template: string) {
     console.log(template);
   }
@@ -79,7 +82,13 @@ export function App() {
             <Separator />
             <div className="space-y-4">
               <Label>Temperatura</Label>
-              <Slider min={0} max={1} step={0.1} />
+              <Slider
+                min={0}
+                max={1}
+                step={0.1}
+                value={[temperature]}
+                onValueChange={(value) => setTemperature(value[0])}
+              />
               <span className="block text-xs text-muted-foreground italic leading-relaxed">
                 Valores mais altos tendem a deixar o resultado mais criativo e
                 com possíveis erros.
