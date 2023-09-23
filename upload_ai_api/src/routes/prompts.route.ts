db45fcf11ccfff122b057d5dea88f396baaa9f09
samsks <samuel.meireles.15s@outlook.com>
2023-09-23 20:45:02 -0300

    docs(backend_api): :memo: add doc for routes

--- upload_ai_api/src/routes/prompts.route.ts
+++ upload_ai_api/src/routes/prompts.route.ts
@@ -1,12 +1,27 @@
-import { FastifyInstance } from "fastify";
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { prisma } from "../lib/prisma";
 
 /**
  * Registers the prompt routes to the Fastify instance.
- * @param app - The Fastify instance to register the routes to.
+ * @param {FastifyInstance} app - The Fastify instance to register the routes to.
+ * @returns {Promise<void>} A promise that resolves when the routes are registered.
  */
-export async function promptRoutes(app: FastifyInstance) {
-  app.get("/", async (request, reply) => {
-    return await prisma.prompt.findMany();
-  });
+export async function promptRoutes(app: FastifyInstance): Promise<void> {
+  /**
+   * GET / - Retrieves all prompts.
+   * @param {FastifyRequest} request - The request object.
+   * @param {FastifyReply} reply - The reply object.
+   * @returns {Promise<FastifyReply>} An array of prompts.
+   */
+  app.get(
+    "/",
+    async (
+      _request: FastifyRequest,
+      reply: FastifyReply
+    ): Promise<FastifyReply> => {
+      const result = await prisma.prompt.findMany();
+
+      return reply.send(result);
+    }
+  );
 }
--- upload_ai_api/src/routes/videos.route.ts
+++ upload_ai_api/src/routes/videos.route.ts
@@ -1,4 +1,4 @@
-import { FastifyInstance } from "fastify";
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import fastifyMultipart from "@fastify/multipart";
 import path from "node:path";
 import { randomUUID } from "node:crypto";
@@ -11,83 +11,130 @@ import { openai } from "../lib/openai";
 
 const pump = promisify(pipeline);
 
-export async function videoRoutes(app: FastifyInstance) {
+/**
+ * Registers the video routes to the Fastify instance.
+ * @param {FastifyInstance} app - The Fastify instance.
+ * @returns {Promise<void>} A promise that resolves when the routes are registered.
+ */
+export async function videoRoutes(app: FastifyInstance): Promise<void> {
+  /**
+   * Registers fastify-multipart plugin to the Fastify instance.
+   * @param {Object} limits - The limits for the plugin.
+   * @param {number} limits.fileSize - The maximum file size allowed in bytes.
+   */
   app.register(fastifyMultipart, {
     limits: {
       fileSize: 1_048_576 * 25, // 25 MB
     },
   });
 
-  app.post("/", async (request, reply) => {
-    const data = await request.file();
+  /**
+   * POST / - Uploads a video file and saves it to the server.
+   * @param {FastifyRequest} request - The request object.
+   * @param {FastifyReply} reply - The reply object.
+   * @returns {Promise<FastifyReply>} - The video data object.
+   */
+  app.post(
+    "/",
+    async (
+      request: FastifyRequest,
+      reply: FastifyReply
+    ): Promise<FastifyReply> => {
+      /**
+       * @typedef {Object} FileData
+       * @property {string} filename - The name of the uploaded file.
+       * @property {NodeJS.ReadableStream} file - The file stream.
+       */
+      const data = await request.file();
 
-    if (!data)
-      return reply.status(400).send({ error: "Missing file uploaded" });
+      if (!data)
+        return reply.status(400).send({ error: "Missing file uploaded" });
 
-    const extension = path.extname(data.filename);
+      const extension = path.extname(data.filename);
 
-    if (extension !== ".mp3")
-      return reply
-        .status(400)
-        .send({ error: "Invalid file type, please upload a MP3." });
+      if (extension !== ".mp3")
+        return reply
+          .status(400)
+          .send({ error: "Invalid file type, please upload a MP3." });
 
-    const fileBaseName = path.basename(data.filename, extension);
-    const fileUploadName = `${fileBaseName}-${randomUUID()}${extension}`;
+      const fileBaseName = path.basename(data.filename, extension);
+      const fileUploadName = `${fileBaseName}-${randomUUID()}${extension}`;
 
-    const uploadDestination = path.resolve(
-      __dirname,
-      "../../tmp",
-      fileUploadName
-    );
+      const uploadDestination = path.resolve(
+        __dirname,
+        "../../tmp",
+        fileUploadName
+      );
 
-    await pump(data.file, fs.createWriteStream(uploadDestination));
+      await pump(data.file, fs.createWriteStream(uploadDestination));
 
-    const videoData = await prisma.video.create({
-      data: {
-        name: data.filename,
-        path: uploadDestination,
-      },
-    });
+      const videoData = await prisma.video.create({
+        data: {
+          name: data.filename,
+          path: uploadDestination,
+        },
+      });
 
-    return reply.status(201).send(videoData);
-  });
+      return reply.status(201).send(videoData);
+    }
+  );
 
-  app.post("/:videoId/transcript", async (request, reply) => {
-    const paramsSchema = z.object({
-      videoId: z.string().uuid(),
-    });
-    const { videoId } = paramsSchema.parse(request.params);
+  /**
+   * POST / - Transcribes a video file using OpenAI's API.
+   * @param {FastifyRequest} request - The request object.
+   * @param {FastifyReply} reply - The reply object.
+   * @returns {Promise<FastifyReply>} - The transcription object.
+   */
+  app.post(
+    "/:videoId/transcription",
+    async (
+      request: FastifyRequest,
+      reply: FastifyReply
+    ): Promise<FastifyReply> => {
+      /**
+       * @typedef {Object} Params
+       * @property {string} videoId - The ID of the video to transcribe.
+       */
+      const paramsSchema = z.object({
+        videoId: z.string().uuid(),
+      });
+      const { videoId } = paramsSchema.parse(request.params);
 
-    const bodySchema = z.object({
-      prompt: z.string(),
-    });
-    const { prompt } = bodySchema.parse(request.body);
+      /**
+       * @typedef {Object} Body
+       * @property {string} prompt - The prompt to use for the transcription.
+       */
+      const bodySchema = z.object({
+        prompt: z.string(),
+      });
+      const { prompt } = bodySchema.parse(request.body);
 
-    const video = await prisma.video.findUniqueOrThrow({
-      where: { id: videoId },
-    });
+      const video = await prisma.video.findUniqueOrThrow({
+        where: { id: videoId },
+      });
 
-    const videoPath = video.path;
-    const audioReadStream = createReadStream(videoPath);
+      const videoPath = video.path;
+      const audioReadStream = createReadStream(videoPath);
 
-    const response = await openai.audio.transcriptions.create({
-      file: audioReadStream,
-      model: "whisper-1",
-      language: "pt",
-      response_format: "json",
-      temperature: 0,
-      prompt,
-    });
+      const response = await openai.audio.transcriptions.create({
+        file: audioReadStream,
+        model: "whisper-1",
+        language: "pt",
+        response_format: "json",
+        temperature: 0,
+        prompt,
+      });
 
-    const transcription = response.text;
+      const transcription = response.text;
 
-    await prisma.video.update({
-      where: { id: videoId },
-      data: {
-        transcription,
-      },
-    });
+      await prisma.video.update({
+        where: { id: videoId },
+        data: {
+          transcription,
+        },
+      });
 
-    return { transcription };
-  });
+      return reply.send({ transcription });
+    }
+  );
 }
